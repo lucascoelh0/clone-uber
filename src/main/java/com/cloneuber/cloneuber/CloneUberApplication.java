fd1b47f5574bedccb51f0793e51c8c7ebecc4de9
Everton Fernando Ferreira Cordeiro <evertonffc37@gmail.com>
2022-05-29 02:23:50 -0300

    Primeiro commit feature motoristas

---
+++ src/main/java/com/cloneuber/cloneuber/CloneUberApplication.java
@@ -0,0 +1,13 @@
+package com.cloneuber.cloneuber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneUberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloneUberApplication.class, args);
+	}
+
+}
---
+++ src/main/java/repository/MotoristaRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import model.Motorista;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+}
